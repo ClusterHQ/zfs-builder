30bda28a1d25d595051733bbc6a4d2a0a421e57c
Luke Marsden <luke@clusterhq.com>
2016-03-31 15:50:17 -0700

    Get the kernel modules out.

--- build-boot2docker.sh
+++ build-boot2docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t clusterhq/build-zfs-boot2docker -f Dockerfile.boot2docker .
 # TODO make the container output the binaries somewhere...
-./zfs-builder docker run -v ${PWD}:/pwd clusterhq/build-zfs-boot2docker
+./zfs-builder docker run -v ${PWD}/rootfs:/rootfs clusterhq/build-zfs-boot2docker /build_zfs.sh
