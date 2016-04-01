31548192bdc7b47bdd5d169c4cdee0d24291c164
Luke Marsden <luke@clusterhq.com>
2016-04-01 05:24:35 -0700

    Oops.

--- build-boot2docker.sh
+++ build-boot2docker.sh
@@ -4,7 +4,7 @@ function build {
     UNAME_R=$2
     docker build --build-arg KERNEL_VERSION=$KERNEL -t clusterhq/build-zfs-boot2docker:${UNAME_R} -f Dockerfile.boot2docker .
     # TODO make the container output the binaries somewhere...
-    ./zfs-builder docker run -e UNAME_R=$UNAME_R -v ${PWD}/rootfs:/rootfs clusterhq/build-zfs-boot3docker:${UNAME_R} /build_zfs.sh
+    ./zfs-builder docker run -e UNAME_R=$UNAME_R -v ${PWD}/rootfs:/rootfs clusterhq/build-zfs-boot2docker:${UNAME_R} /build_zfs.sh
 }
 
 # look up docker version -> kernel mapping here:
