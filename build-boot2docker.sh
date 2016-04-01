511606411e6b1ce7c130ad4eb3cde2180294dbc8
Luke Marsden <luke@clusterhq.com>
2016-03-31 18:37:18 -0700

    tag it

--- build-boot2docker.sh
+++ build-boot2docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t clusterhq/build-zfs-boot2docker -f Dockerfile.boot2docker .
+docker build -t clusterhq/build-zfs-boot2docker:docker-1.8.1-linux-4.0.9 -f Dockerfile.boot2docker .
 # TODO make the container output the binaries somewhere...
 ./zfs-builder docker run -v ${PWD}/rootfs:/rootfs clusterhq/build-zfs-boot2docker /build_zfs.sh
