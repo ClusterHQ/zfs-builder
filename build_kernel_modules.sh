8c978782013211393680fa16916cf1c461d6c956
Luke Marsden <luke@clusterhq.com>
2016-04-01 20:26:50 -0700

    Looks like the env var and the config var are detected. Just use the env var.

--- build_kernel_modules.sh
+++ build_kernel_modules.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 function build {
-    echo Building $1 $2
+    echo Building $1 $2 $3
     KERNEL=$1
     UNAME_R=$2
-    docker build --build-arg KERNEL_VERSION=$KERNEL -t clusterhq/build-zfs-boot2docker:${UNAME_R} -f Dockerfile.boot2docker .
+    LOCALVERSION=$3
+    docker build --build-arg KERNEL_VERSION=$KERNEL --build-arg LOCALVERSION="-$LOCALVERSION" -t clusterhq/build-zfs-boot2docker:${UNAME_R} -f Dockerfile.boot2docker .
     UNAME_R=$UNAME_R ./zfs-builder sh -c "docker run -e UNAME_R=$UNAME_R -v ${PWD}/rootfs:/rootfs clusterhq/build-zfs-boot2docker:${UNAME_R} /build_zfs.sh && cp rootfs/zfs-${UNAME_R}.tar.gz ."
 }
 
@@ -11,19 +12,19 @@ function build {
 # look up docker version -> kernel mapping here:
 # https://github.com/boot2docker/boot2docker/releases
 
-build 4.1.19 4.1.19-boot2docker
+build 4.1.19 4.1.19-boot2docker boot2docker
 
-build 4.1.18 4.1.18-boot2docker
-build 4.1.17 4.1.17-boot2docker
+build 4.1.18 4.1.18-boot2docker boot2docker
+build 4.1.17 4.1.17-boot2docker boot2docker
 
-build 4.1.13 4.1.13-boot2docker
-build 4.1.12 4.1.12-boot2docker
+build 4.1.13 4.1.13-boot2docker boot2docker
+build 4.1.12 4.1.12-boot2docker boot2docker
 
-build 4.0.10 4.0.10-boot2docker
-build 4.0.9 4.0.9-boot2docker
+build 4.0.10 4.0.10-boot2docker boot2docker
+build 4.0.9 4.0.9-boot2docker boot2docker
 
-# travis trusty
-build 3.19 3.19.0-30-generic
+# travis trusty XXX '30-generic' is a guess
+build 3.19 3.19.0-30-generic 30-generic
 
 # travis precise, probably an ubuntu kernel
 #build 3.13 3.13.0-63-generic # XXX fails
