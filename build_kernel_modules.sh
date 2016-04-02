cc647f5c690bf9bf4c1cb193d27a56b05e5514e8
Luke Marsden <luke@clusterhq.com>
2016-04-02 10:06:44 -0700

    comment out one we know won't work

--- build_kernel_modules.sh
+++ build_kernel_modules.sh
@@ -23,8 +23,9 @@ build 4.1.12 4.1.12-boot2docker boot2docker
 build 4.0.10 4.0.10-boot2docker boot2docker
 build 4.0.9 4.0.9-boot2docker boot2docker
 
-# travis trusty XXX this won't work because "-boot2docker" is in kernel_config here
-build 3.19 3.19.0-30-generic ubuntu-trusty
+# travis trusty XXX TODO create Dockerfile.ubuntu-trusty and
+# kernel_config.ububtu-trusty
+#build 3.19 3.19.0-30-generic ubuntu-trusty
 
 # travis precise, probably an ubuntu kernel
 #build 3.13 3.13.0-63-generic ubuntu-precise # XXX fails
